a235e5f78feb770e771ae1f45a428cc4c19c7e3a
Md Erfan Ahammed Santo <erfanahammed990@gmail.com>
2022-12-26 11:58:29 +0600

    Data Structure and Algorithm

---
+++ BinarySearch/src/InfiniteArray.java
@@ -0,0 +1,9 @@
+public class InfiniteArray {
+
+    // https://www.geeksforgeeks.org/find-position-element-sorted-array-infinite-numbers/
+    public static void main(String[] args) {
+
+    }
+
+    
+}
